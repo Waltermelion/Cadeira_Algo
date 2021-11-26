d6c997234e1b147c4d951a5a7e56df8979d5ea43
Waltermelion <guilhermelpmeneses@gmail.com>
2021-11-26 10:24:42 +0000

    Initial commit

---
+++ Ex_sentences.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstword = "test";
+            string lastword = "yes";
+            string space = " ";            
+
+            Console.WriteLine("Write the sentence.");
+            string sentence = Console.ReadLine();
+
+
+            if (firstword == lastword)
+            {
+                Console.WriteLine("The first and the last word are the same: ");
+                Console.Write(firstword);
+            }
+            else
+            {
+                Console.WriteLine("The first and the last word are not the same");
+            }
+            
+                      
+        }
+    }
+}
