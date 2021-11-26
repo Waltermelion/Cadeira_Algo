ed9e6d617e8de53114fd2eeab38fb6309b2d8465
Waltermelion <guilhermelpmeneses@gmail.com>
2021-11-26 12:24:18 +0000

    List_ex3 start

---
+++ List_ex3/List_ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("A list of numbers");
+            List<int> numbers = new List<int>(new int[] { 65, 47, 100, 3, 19 });
+            Console.WriteLine(string.Join(", ", numbers));
+
+        }
+    }
+}
