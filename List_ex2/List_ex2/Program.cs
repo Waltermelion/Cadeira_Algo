052cfdff294d7459b27bbc85533cf7cd7cdf5d8f
Waltermelion <guilhermelpmeneses@gmail.com>
2021-11-26 11:47:14 +0000

    Create new project

---
+++ List_ex2/List_ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("A list of words");
+            List<string> palavras = new List<string>(10);
+            for (int i = 0; i < palavras.Capacity; i++)
+            {
+                palavras.Add(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join(", ", palavras));
+
+        }
+    }
+}
