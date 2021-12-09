10fb98161a9247dca242e88302ca17b6091435eb
Waltermelion <guilhermelpmeneses@gmail.com>
2021-12-09 23:55:01 +0000

    Update Program.cs

--- Dados_dnd/Program.cs
+++ Dados_dnd/Program.cs
@@ -1,68 +1,41 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Threading;
 
-namespace testbed3
+namespace ConsoleApp2
 {
+     
     class Program
     {
+        public static int counter = 0;
+        public static int counterall = 0;
+        public static int[] results = new int[9000];
+        //usar enum para criar a lista das armas para usar no switch linha 144
+        public enum Weapon_Type
+        {
+            Dagger,
+            Shortsword,
+            Battleaxe
+        };
+        public static Weapon_Type weapon_Type;
         public static int close = 1;
-        public static int em = -1;
+
         static void Main(string[] args) // Main Function
         {
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("Escolha uma opcao:\n1> Roll Dice\n2> DND\n0>Sair");
-                em = Convert.ToInt32(Console.ReadLine());
-                switch (em)
-                {
-                    case 1:
-                        {
-                            Console.Clear();
-                            Main2();
-                            break;
-                        }
-
-                    case 2:
-                        {
-                            Console.Clear();
-                            BasicAttack();
-                            break;
-                        }
-
-                    case 0:
-                        {
-
-                            break;
-                        }
-
-                    default:
-                        {
-                            Console.Clear();
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Escolha uma opcao valida!");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            break;
-                        }
-                }
-            }
-            while (em != 0);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Prima uma tecla para terminar");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.ReadKey();
-        }
-
-        //funcao para user input dados apenas
-        public static int Main2()
-        {
+        
             int dados, qdados;
+             
+            int[] dadosarray = { 4, 6, 8, 10, 12, 20, 100 };//array
             do
-            {
+            { 
                 Console.WriteLine("Introduza um numero: "); // 4, 6, 8, 10, 12, 20 ou 100
                 dados = Convert.ToInt32(Console.ReadLine());
                 Console.Clear();
 
-                if (dados == 4 || dados == 6 || dados == 8 || dados == 10 || dados == 12 || dados == 20 || dados == 100)
+                int pos = Array.IndexOf(dadosarray, dados); //criar variavel para guardar a posicao do array
+
+                if (pos > -1 /*verificar existencia de dados no array, faz o mesmo que: if(dados == 4 || dados == 6 || dados == 8 || dados == 10 || dados == 12 || dados == 20 || dados == 100)*/)
                 {
                     do
                     {
@@ -84,10 +57,9 @@ namespace testbed3
                         }
                         else
                         {
-                            qdados = Rolldice(dados, qdados);
+                            close = BasicAttack(qdados);
                         }
-                    }
-                    while (close != 0);
+                    }while (close != 0);
                 }
                 else if (dados == 0)
                 {
@@ -103,29 +75,25 @@ namespace testbed3
                 }
             }
             while (close != 0);
-            return 0;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Pressione uma tecla para sair");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
         }
 
         //funcao para rodar e calcular (parte da main2)
         public static int Rolldice(int x, int w)
         {
             int soma = 0;
-            int random = 0;
-            Random r = new Random();
+            int random;
+            Random r = new();
 
-            for (int i = 1; i <= w; i = i + 1)
+            for (int i = 1; i <= w; i++)
             {
                 random = r.Next(1, x + 1);
-
-                if (em == 1)
-                {
-                    soma += random;
-                    Console.WriteLine($"O valor da face {i} é: {random}");
-                }
+                soma += random;
+                Console.WriteLine($"O valor da face {i} é: {random}");
             }
-
-            if(em == 1)
-            {
                 Console.WriteLine($"A soma das faces e: {soma}");
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("pressione enter para continuar");
@@ -134,47 +102,76 @@ namespace testbed3
                 Console.Clear();
                 close = 0;
                 return w;
-            }
-            return random;
         }
 
         //funcao para o roll + basic atk + damage
-        public static void  BasicAttack()
+        public static int  BasicAttack(int y)
         {
+            int valorcustom;
             Console.Clear();
-            Console.WriteLine("Escolhe a tua arma: Dagger: 1, Shortsword: 2, Battleaxe: 3");
-            int arma = Convert.ToInt32(Console.ReadLine());
-            int attk = Rolldice(20, 1);
-            int dmg = 0;
-            if (attk >= 10)
+            Console.WriteLine("Escolhe a tua arma: Dagger, Shortsword, Battleaxe");
+            
+            string compare = Console.ReadLine();
+            if (compare == "Dagger") {
+
+            } else if (compare == "Shortsword") {
+
+            } else if (compare == "Battlesword") {
+
+            }
+            else
+            {
+               Console.WriteLine("Por favor introduza um valor correto");
+            }
+
+            //exercicio 2 da lesson 6, nao perguntamos ao user o nr de dots.
+            Console.Clear();
+            Console.WriteLine("Introduza o numero de vezes que quer rolar");
+            valorcustom = Console.Read();
+            int attk = Rolldice(20/*nr da face fixo*/, valorcustom/*nr de rolls varia no ex2 lesson 6*/) ;
+            int dmg;
+
+            //adicionar AC | modificador de damage perguntando ao user
+            Console.WriteLine("Escolhe um valor para o dmg.multiplicador(superior a 10)");
+            int AC = Convert.ToInt32(Console.ReadLine());
+            results[counterall] = attk;
+            counterall++; //all count
+            if (attk >= AC)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"Result {attk}: Attack succeeded, rolling for damage…\n");
-                Console.ForegroundColor = ConsoleColor.White;
-                switch (arma)
+                counter++; //successful count
+                Console.ForegroundColor = ConsoleColor.White; 
+
+                switch (weapon_Type)
                 {
 
-                    case 1:
+                    case Weapon_Type.Dagger:
                         {
-                            dmg = Rolldice(4, 1);
-                            Console.WriteLine($"Attack resulted in {dmg} points of damage.\nPrima uma tecla para continuar");
+                            dmg = AC + Rolldice(4, 1);
+                            Console.WriteLine($"Attack resulted in {dmg} points of damage.\nAverage:{results.Average()}\nCounter:{counter}\nCounterAll:{counterall}\nPrima uma tecla para continuar");
                             Console.ReadKey();
                             break;
                         }
-                    case 2:
+                    case Weapon_Type.Shortsword:
                         {
-                            dmg = Rolldice(6, 1);
-                            Console.WriteLine($"Attack resulted in {dmg} points of damage.\nPrima uma tecla para continuar");
+                            dmg = AC + Rolldice(6, 1);
+                            Console.WriteLine($"Attack resulted in {dmg} points of damage.\nAverage:{results.Average()}\nCounter(success):{counter}\nCounterAll:{counterall}\nPrima uma tecla para continuar");
                             Console.ReadKey();
                             break;
                         }
-                    case 3:
+                    case Weapon_Type.Battleaxe:
                         {
-                            dmg = Rolldice(8, 1);
-                            Console.WriteLine($"Attack resulted in {dmg} points of damage.\nPrima uma tecla para continuar");
+                            dmg = AC + Rolldice(8, 1);
+                            Console.WriteLine($"Attack resulted in {dmg} points of damage.\nAverage:{results.Average()}\nCounter:{counter}\nCounterAll:{counterall}\nPrima uma tecla para continuar");
                             Console.ReadKey();
                             break;
                         }
+                    default:
+                    {
+                        Console.WriteLine("Por favor introduz uma arma valida.\n");
+                        break;
+                    }
                 }
             }
             else if (attk <= 10)
@@ -184,7 +181,9 @@ namespace testbed3
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.ReadKey();
                 Console.Clear();
+                //close = 1;
             }
+            return 1;
         }
 
     }
