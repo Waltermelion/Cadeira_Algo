2817fdbe5be478ab85ffe114b5e8f0b0482603c3
Waltermelion <guilhermelpmeneses@gmail.com>
2021-12-17 12:38:32 +0000

    create bubble sort

---
+++ Bubble_sort/Bubble_sort/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bubble_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Press any button to generate a random array of numbers, and lets see the bubble sort working");
+            int[] arrayNum = new int[] { 9, 3, 2, 7, 1 };
+            for (int i = 0; i < 5; i++)
+            {
+                
+            }
+        }
+    }
+}
